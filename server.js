6b1b9caf2c0214a2ee9bf3a34312d20deb5cebae
xnwindows <admin@xnwindows.au>
2025-07-23 15:45:23 +1000

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/preview', (req, res) => {
   const row = (label, value) => `<tr><td>${label}</td><td>${value}</td></tr>`;
 
   html = html.replace('{{delivery_fee_row}}', latestData.delivery_fee ? row('Delivery Fee', latestData.delivery_fee) : '');
-  html = html.replace('{{demolition_fee_row}}', latestData.demolition_fee ? row('Demolition / Rubbish Removal', latestData.demolition_fee) : '');
+  html = html.replace('{{demolition_fee_row}}', latestData.demolition_fee ? row('Demolition Fee', latestData.demolition_fee) : '');
   html = html.replace('{{installation_fee_row}}', latestData.installation_fee ? row('Installation Fee', latestData.installation_fee) : '');
   html = html.replace('{{disposal_fee_row}}', latestData.disposal_fee ? row('Waste Disposal Fee', latestData.disposal_fee) : '');
   html = html.replace('{{gst_cost_row}}', latestData.gst_cost ? row('GST (10%)', latestData.gst_cost) : '');
